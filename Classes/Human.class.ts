77c6231415d6e1440d4495280a99cf9265ae41af
VovaYarosh <vovayaroshlenovo94@gmail.com>
2019-10-30 15:58:20 +0200

    check

---
+++ Classes/Human.class.ts
@@ -0,0 +1,8 @@
+export class Human{
+    height: number
+    weight: number
+    constructor(height: number, weight: number) {
+        this.height = height;
+        this.weight = weight;
+    }
+}
